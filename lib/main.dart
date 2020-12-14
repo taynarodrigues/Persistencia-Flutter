a610f841108ca44bed0af469fe3e030ee17e6aa2
Tayna Rodrigues <taynajesus@gmail.com>
2020-12-14 14:38:01 -0400

    Adicionando Imagem e botão de contatos

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,28 @@ class BytebankApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard'),),
-      )
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: [
+                  Icon(Icons.people),
+                  Text('Contacts')
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
