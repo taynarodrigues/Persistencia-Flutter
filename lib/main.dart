eb099dbc3ee451513796c798f1187b0574a64ae1
Tayna Rodrigues <taynajesus@gmail.com>
2020-12-15 16:07:21 -0400

    Inserindo e buscando informações

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
-import 'package:ByteBankApp/screens/contact_form.dart';
-import 'package:ByteBankApp/screens/contact_list.dart';
+
 import 'package:ByteBankApp/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(BytebankApp());
-}
+import 'database/app_database.dart';
+import 'models/contact.dart';
+
+  void main() {
+    runApp(BytebankApp());
+    save(Contact(2, 'tayna', 10100)).then((id){
+      findAll().then((contacts) => debugPrint(contacts.toString()));
+     });
+  }
+
 
 class BytebankApp extends StatelessWidget {
   // This widget is the root of your application.
